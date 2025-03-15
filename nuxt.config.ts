a54568cd795225452d22ae4ec317544ae73ba1b1
Tanapong <stanapong@monogps.com>
2025-03-15 11:30:20 +0700

    add auto import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
-  modules: ["@pinia/nuxt"],
+  modules: [["@pinia/nuxt", { autoImports: ["defineStore"] }]],
 });
