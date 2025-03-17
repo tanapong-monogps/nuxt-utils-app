f13cfc0cd95987f102903fae439c468d9ecf5e9c
Tanapong <stanapong@monogps.com>
2025-03-17 08:49:45 +0700

    remove unuse code

--- app.vue
+++ app.vue
@@ -4,10 +4,3 @@
     <NuxtWelcome />
   </div>
 </template>
-<script setup lang="ts">
-const { setBaseUrl, onResponse, onResponseError, headers } =
-  useApiGlobalConfig();
-setBaseUrl("https://fakestoreapi.com");
-headers().add("Authorization", "Bearer token");
-headers().add("x-auth", "test auth");
-</script>
